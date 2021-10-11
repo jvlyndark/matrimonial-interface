8d128658a16f2bbda92e2f10052cb81ae72975f8
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2021-10-11 14:45:01 -0400

    edits to users

--- matrimonial-interface/app/models/profile.rb
+++ matrimonial-interface/app/models/profile.rb
@@ -1,3 +1,4 @@
 class Profile < ApplicationRecord
     validates :name, presence: true
+    belongs_to :user, class_name: "user", foreign_key: "user_id"
 end
--- matrimonial-interface/app/models/user.rb
+++ matrimonial-interface/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
     has_secure_password
-    validates_uniqueness_of :email
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: VALID_EMAIL_REGEX}
     has_one :profile
     has_many :images
     after_create :build_profile
