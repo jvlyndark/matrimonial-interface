23ec828d7f9e64431498db925d771895f6428bd4
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2021-10-11 17:22:55 -0400

    allow users to edit account info

--- matrimonial-interface/app/controllers/users_controller.rb
+++ matrimonial-interface/app/controllers/users_controller.rb
@@ -2,6 +2,20 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your account has been successfully updated!"
+            redirect_to profiles_path
+        else
+            render 'edit'
+        end
+    end
       
     def create
         @user = User.new(user_params)
