0ce97d2c07d96e7041e5e4040b1b757957234795
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2021-10-11 15:35:59 -0400

    one-to-one-associations

--- matrimonial-interface/app/models/profile.rb
+++ matrimonial-interface/app/models/profile.rb
@@ -1,4 +1,4 @@
 class Profile < ApplicationRecord
     validates :name, presence: true
-    belongs_to :user, class_name: "user", foreign_key: "user_id"
+    belongs_to :user, class_name: "User", foreign_key: "user_id"
 end
