6870706f19a38c1b162b0ed137f34c8baf0cbfb7
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2021-10-12 19:32:58 -0400

    update profiles db with new columns

--- matrimonial-interface/db/schema.rb
+++ matrimonial-interface/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_212155) do
+ActiveRecord::Schema.define(version: 2021_10_12_232235) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,9 @@ ActiveRecord::Schema.define(version: 2021_10_04_212155) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "age"
+    t.string "gender"
+    t.string "location"
   end
 
   create_table "user_profiles", force: :cascade do |t|
